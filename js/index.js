7e45ec9f6ba5112cf1a3cc4276020d56d8b7548d
Paul M. Summitt <psummitt@gmail.com>
2022-11-18 19:54:38 -0600

    day 2

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 var canvas;
 var canvasContext;
-var ballX = 50
+var ballX = 50;
+var ballY = 50;
+var ballSpeedX = 5;
+var ballSpeedY = 5;
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
@@ -14,17 +17,32 @@ window.onload = function() {
 }
 
 function moveEverything() {
-    ballX += 5;
-
+    ballX += ballSpeedX;
+    if(ballX < 0) {
+        ballSpeedX = -ballSpeedX;
+    }
+    if(ballX > canvas.width) {
+        ballSpeedX = -ballSpeedX;
+    }
 }
 
-
 function drawEverything() {
+    // next line blanks out the screen with black
+    colorRect(0.0.canvas.width, canvas.height, 'black');
+    // this is the left player paddle
+    colorRect(0,210,10,100, 'white');
+    // next line draws the ball
+    colorCircle(ballX, 150, 10, 'white');
+}
 
-    canvasContext.fillstyle = 'black';
-    canvasContext.fillRect(0.0.canvas.width, canvas.height);
-    canvasContext.fillstyle = 'white';
-    canvasContext.fillRect(0,210,10,100);
-    canvasContext.fillstyle = 'red';
-    canvasContext.fillRect(ballX,200,10,10);
+function colorCircle(centerX, centerY, radius, drawColor) {
+    canvasContext.fillstyle = drawColo;
+    canvasContext.beginPath();
+    canvasContext.arc(centerX, centerY, radius, 0, Math.Pi*2, true);
+    canvasContext.fill();
+}
+
+function colorRect(leftX, topY, width, height, drawColor) {
+    canvasContext.fillstyle = drawColor;
+    canvasContext.fillRect(leftX, topY, width, height);
 }
