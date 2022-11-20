6e4cca8b5913690dfdca1c575e9e0fe19d8dd81c
Paul M. Summitt <psummitt@gmail.com>
2022-11-20 14:33:27 -0600

    player paddle

--- js/index.js
+++ js/index.js
@@ -2,8 +2,22 @@ var canvas;
 var canvasContext;
 var ballX = 50;
 var ballY = 50;
-var ballSpeedX = 5;
-var ballSpeedY = 5;
+var ballSpeedX = 10;
+var ballSpeedY = 4;
+
+var paddle1Y = 250;
+const PADDLE_HEIGHT = 100;
+
+function calculateMousePos(evt) {
+    var rect = canvas.getBoundingClientRect();
+    var root = document.documentElement;
+    var mouseX = evt.clientX - rect.left - root.scrollLeft;
+    var mouseY = evt.clientY - rect.Top - root.scrollTop;
+    return {
+        x:mouseX,
+        y:mouseY
+    };
+}
 
 window.onload = function() {
     canvas = document.getElementById('gameCanvas');
@@ -14,29 +28,43 @@ window.onload = function() {
         moveEverything();
         drawEverything();
     }, 1000/framesPerSecond);
+
+    canvas.addEventListener('mousemove',
+        function(evt) {
+            var mousePos = calculateMousePos(evt);
+            paddle1Y = mousePos.y - (PADDLE_HEIGHT/2);
+        })
+
 }
 
 function moveEverything() {
-    ballX += ballSpeedX;
+    ballX = ballX + ballSpeedX;
+    ballY = ballY + ballSpeedY;
     if(ballX < 0) {
         ballSpeedX = -ballSpeedX;
     }
     if(ballX > canvas.width) {
         ballSpeedX = -ballSpeedX;
     }
+    if(ballY < 0) {
+        ballSpeedY = -ballSpeedY;
+    }
+    if(ballY > canvas.height) {
+        ballSpeedY = -ballSpeedY;
+    }
 }
 
 function drawEverything() {
     // next line blanks out the screen with black
-    colorRect(0.0.canvas.width, canvas.height, 'black');
+    colorRect(0.0,canvas.width, canvas.height, 'black');
     // this is the left player paddle
-    colorRect(0,210,10,100, 'white');
+    colorRect(0,paddle1Y,10,PADDLE_HEIGHT, 'white');
     // next line draws the ball
-    colorCircle(ballX, 150, 10, 'white');
+    colorCircle(ballX, ballY, 10, 'white');
 }
 
 function colorCircle(centerX, centerY, radius, drawColor) {
-    canvasContext.fillstyle = drawColo;
+    canvasContext.fillstyle = drawColor;
     canvasContext.beginPath();
     canvasContext.arc(centerX, centerY, radius, 0, Math.Pi*2, true);
     canvasContext.fill();
